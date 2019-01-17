2cc527e6ac0dc8a19319e1def29879f4f50fff8d
datdevboi <cami172013@gmail.com>
2019-01-16 20:42:58 -0600

    created a register resolver

--- server/src/modules/register/resolvers.ts
+++ server/src/modules/register/resolvers.ts
@@ -1,11 +1,30 @@
+import jwt from "jsonwebtoken";
 import { ResolverMap } from "../../types";
+import bcrypt from "bcryptjs";
 
 export const resolvers: ResolverMap = {
   Mutation: {
-    register: async (_, args, { prisma }) => {
+    register: async (_, args, { prisma, res }) => {
+      console.log(args);
+      // hash the password
+      const password = await bcrypt.hash(args.input.password, 10);
+
       const user = await prisma.createUser({
-        email: args.email,
-        password: args.password
+        email: args.input.email,
+        password
+      });
+
+      // make secret key in env
+      const token = jwt.sign(
+        {
+          userId: user.id
+        },
+        "randomtokenapp"
+      );
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 365
       });
 
       return user;
