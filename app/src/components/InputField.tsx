4fa0efbbe878e701f21412f9e67f8727c06a6363
datdevboi <cami172013@gmail.com>
2019-01-18 21:54:40 -0600

    fixed a bug in InputField and used TextField from new ui lib

--- app/src/components/InputField.tsx
+++ app/src/components/InputField.tsx
@@ -1,17 +1,19 @@
 import * as React from "react";
-import { Input } from "react-native-elements";
+
+import { TextField as Input } from "react-native-ui-lib";
 import { FieldProps } from "formik";
 
 export class InputField extends React.Component<
   FieldProps<any> & {
     name: string;
+    value: string;
   }
 > {
   onChangeText = (text: string) => {
-    this.props.form.setFieldValue(this.props.name, text);
+    this.props.form.setFieldValue(this.props.field.name, text);
   };
   render() {
-    const { field, form, ...props } = this.props;
+    const { field, form, value, ...props } = this.props;
 
     return (
       <Input {...props} onChangeText={this.onChangeText} value={field.value} />
