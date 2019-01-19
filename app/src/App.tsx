bbcb807d3bd2079e861014951538e7e449beeb3d
datdevboi <cami172013@gmail.com>
2019-01-18 21:55:38 -0600

    Made use of react-navigator

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,27 @@
 import * as React from "react";
 import { Text } from "react-native";
 import { ApolloProvider } from "react-apollo";
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
 import { client } from "./client";
+import { Login } from "./modules/login/";
+import { Register } from "./modules/register";
+
+const AppSwitchNavigator = createSwitchNavigator({
+  Login: {
+    screen: Login
+  },
+  Register: {
+    screen: Register
+  }
+});
+
+const MainScreen = createAppContainer(AppSwitchNavigator);
 
 export class App extends React.Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <Text>Hello</Text>
+        <MainScreen />
       </ApolloProvider>
     );
   }
