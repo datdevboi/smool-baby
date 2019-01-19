3db81e801dde3bc64f88bc4d2451ded27d7c3011
datdevboi <cami172013@gmail.com>
2019-01-19 14:55:58 -0600

    updated our httplink, fixed our uri function

--- app/src/client.ts
+++ app/src/client.ts
@@ -2,9 +2,11 @@ import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
+const host = "http://10.0.0.87:4000/graphql";
+
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:4000"
+    uri: host
   }),
   cache: new InMemoryCache()
 });
