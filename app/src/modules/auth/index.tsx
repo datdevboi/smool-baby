a882cf8c1d3307af4d3701efae846393e3c3755a
datdevboi <cami172013@gmail.com>
2019-01-22 16:59:57 -0600

    created AuthLoadingScreen component this will check if there is a userId in storage if not it will redirect user to login otherwise to the dashboard

---
+++ app/src/modules/auth/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+import { LoaderScreen } from "react-native-ui-lib";
+import { SecureStore } from "expo";
+
+export class AuthLoadingScreen extends React.Component<any> {
+  constructor(props) {
+    super(props);
+
+    this._bootstrapAsync();
+  }
+
+  _bootstrapAsync = async () => {
+    const userToken = await SecureStore.getItemAsync("sid");
+
+    this.props.navigation.navigate(userToken ? "Dashboard" : "Login");
+  };
+  render() {
+    return <LoaderScreen loaderColor="blue" message="Loading..." />;
+  }
+}
