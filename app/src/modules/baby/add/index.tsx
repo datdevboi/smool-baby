a0ed060730653725e118db0264eec56358601542
datdevboi <cami172013@gmail.com>
2019-01-24 15:51:41 -0600

    added function to go back to dashboard

--- app/src/modules/baby/add/index.tsx
+++ app/src/modules/baby/add/index.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import { NavigationScreenProp } from "react-navigation";
 
-export class AddBaby extends React.Component {
+export class AddBaby extends React.Component<any> {
   static navigationOptions = {
     headerMode: "screen"
   };
@@ -16,6 +17,10 @@ export class AddBaby extends React.Component {
         }}
       >
         <Text> Add baby </Text>
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.navigate.goBack()}
+        />
       </View>
     );
   }
