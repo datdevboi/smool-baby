086174197435b91847f4da7b5aa751fbb61fc467
datdevboi <cami172013@gmail.com>
2019-01-25 14:34:21 -0600

    Updated Baby Description, get it's own props and the props for the babyImage

--- app/src/components/BabyDescription.tsx
+++ app/src/components/BabyDescription.tsx
@@ -1,8 +1,32 @@
 import * as React from "react";
+import { View, StyleSheet, Text } from "react-native";
 import { BabyImage } from "./BabyImage";
 
-export class BabyDescription extends React.Component {
+interface Props {
+  imageSrc: any;
+  babyName: String;
+  date: String;
+  style?: any;
+}
+
+export class BabyDescription extends React.Component<Props> {
   render() {
-    return <BabyImage />;
+    return (
+      <View style={[styles.container, this.props.style]}>
+        <BabyImage src={this.props.imageSrc} />
+        <View>
+          <Text>{this.props.babyName}</Text>
+          <Text>{this.props.date}</Text>
+        </View>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between"
+  }
+});
