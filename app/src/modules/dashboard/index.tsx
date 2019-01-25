cd253e8d7b3c0586c7e3718e55e930bc8615cdf6
datdevboi <cami172013@gmail.com>
2019-01-24 18:39:05 -0600

    updated our styles, and rendered BabyImage

--- app/src/modules/dashboard/index.tsx
+++ app/src/modules/dashboard/index.tsx
@@ -1,31 +1,40 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
-
-import { Ionicons } from "@expo/vector-icons";
 import {
-  NavigationScreenProp,
-  NavigationNavigatorProps
-} from "react-navigation";
+  Text,
+  Button,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Platform
+} from "react-native";
+
+import { BabyImage } from "../../components/BabyImage";
 
 export class DashBoard extends React.Component<any> {
   render() {
     return (
-      <View
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flex: 1
-        }}
-      >
+      <SafeAreaView style={styles.mainView}>
         <Text>Hello from Dashboard</Text>
+        <BabyImage />
         <Button
           title="Add baby"
           onPress={() => this.props.navigation.navigate("AddBaby")}
         >
           Add baby
         </Button>
-      </View>
+      </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  mainView: {
+    display: "flex",
+    flex: 1,
+    ...Platform.select({
+      android: {
+        marginTop: StatusBar.currentHeight
+      }
+    })
+  }
+});
