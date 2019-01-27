140116909ef51a3867b444d1d0af60beda482f17
datdevboi <cami172013@gmail.com>
2019-01-27 17:53:48 -0600

    added ribbonLabel to Avatar by adding babyName propand turned the size to 70

--- app/src/components/BabyImage.tsx
+++ app/src/components/BabyImage.tsx
@@ -3,19 +3,22 @@ import { Image, Avatar } from "react-native-ui-lib";
 
 interface Props {
   src: any;
+  babyName?: string;
   style?: any;
 }
 export class BabyImage extends React.Component<Props> {
   render() {
     return (
       <Avatar
-        imageStyle={[
-          {
-            width: 60,
-            height: 60
-          },
-          this.props.style
-        ]}
+        ribbonLabel={this.props.babyName}
+        size={70}
+        // imageStyle={[
+        //   {
+        //     width: 60,
+        //     height: 60
+        //   },
+        //   this.props.style
+        // ]}
         imageSource={{ uri: this.props.src }}
       />
     );
