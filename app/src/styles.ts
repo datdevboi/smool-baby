6238e99fa11bcd67b4c2ec0b729fac358547e9ee
datdevboi <cami172013@gmail.com>
2019-01-28 13:59:21 -0600

    created styles file with Styles object for our global styles

---
+++ app/src/styles.ts
@@ -0,0 +1,9 @@
+import { Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+const STYLES = {
+  colors: {},
+  DEVICE_WIDTH: width,
+  DEVICE_HEIGHT: height
+};
