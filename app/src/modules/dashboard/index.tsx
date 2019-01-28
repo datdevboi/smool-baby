959bc0116e2c41f73fc63e7ec6a2a74cb3c964a8
datdevboi <cami172013@gmail.com>
2019-01-28 14:00:22 -0600

    created a basic modal view

--- app/src/modules/dashboard/index.tsx
+++ app/src/modules/dashboard/index.tsx
@@ -1,17 +1,36 @@
 import * as React from "react";
 import {
   Text,
-  Button,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Platform,
   View
 } from "react-native";
+import { BlurView } from "expo";
+import { Button, Card, Modal } from "react-native-ui-lib";
 
 import { BabyDescription } from "../../components/BabyDescription";
 
+const BR = (
+  <BlurView tint="light" intensity={50} style={StyleSheet.absoluteFill} />
+);
 export class DashBoard extends React.Component<any> {
+  state = {
+    modalOpen: false
+  };
+
+  openModal = () => {
+    this.setState({
+      modalOpen: true
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      modalOpen: false
+    });
+  };
   render() {
     return (
       <SafeAreaView style={styles.mainView}>
@@ -19,19 +38,32 @@ export class DashBoard extends React.Component<any> {
           imageSrc={
             "https://www.momjunction.com/wp-content/uploads/2014/05/Sweet-Cute-Baby-Girl-Names-With-Meanings.jpg"
           }
-          babyName="Ivy"
-          date="11/19/2018"
+          babyName={"Ivy"}
+          handlePress={this.openModal}
           style={{ flex: 2 }}
         />
         <View style={styles.listView}>
           <Text>List</Text>
         </View>
-        {/* <Button
-          title="Add baby"
-          onPress={() => this.props.navigation.navigate("AddBaby")}
+
+        <Modal
+          visible={this.state.modalOpen}
+          onRequestClose={this.closeModal}
+          transparent={true}
+          animationType="slide"
         >
-          Add baby
-        </Button> */}
+          <BlurView
+            tint="light"
+            intensity={100}
+            style={styles.modalViewContainer}
+          >
+            <View>
+              <Button onPress={this.closeModal}>
+                <Text>Close modal</Text>
+              </Button>
+            </View>
+          </BlurView>
+        </Modal>
       </SafeAreaView>
     );
   }
@@ -50,5 +82,11 @@ const styles = StyleSheet.create({
   listView: {
     flex: 10,
     backgroundColor: "#f6f1ed"
+  },
+  modalViewContainer: {
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 100
   }
 });
