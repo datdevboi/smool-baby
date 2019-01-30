bb0d500a4872bca251a6249698539bf43e699168
datdevboi <cami172013@gmail.com>
2019-01-30 12:43:02 -0600

    Imported Query and gql to create. We created  a query that will fetch the current user and it's babies

--- app/src/components/BabyModal.tsx
+++ app/src/components/BabyModal.tsx
@@ -1,7 +1,30 @@
 import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  TouchableWithoutFeedback,
+  Alert
+} from "react-native";
 import { BlurView } from "expo";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 import { Modal, Button } from "react-native-ui-lib";
+import { Ionicons } from "@expo/vector-icons";
+
+const OS = Platform.OS;
+
+const ME_QUERY = gql`
+  query {
+    me {
+      email
+      babies {
+        name
+      }
+    }
+  }
+`;
 
 interface Props {
   isOpen: boolean;
@@ -17,10 +40,34 @@ export const BabyModal: React.SFC<Props> = ({ isOpen, closeModal }) => {
       animationType="slide"
     >
       <BlurView tint="light" intensity={80} style={styles.modalViewContainer}>
+        <View style={styles.closeView}>
+          <TouchableWithoutFeedback onPress={closeModal}>
+            <Ionicons
+              size={25}
+              name={OS === "ios" ? "ios-close-circle" : "md-close-circle"}
+            />
+          </TouchableWithoutFeedback>
+        </View>
+
         <View>
-          <Button onPress={closeModal}>
-            <Text>Close modal</Text>
-          </Button>
+          <Query query={ME_QUERY}>
+            {({ data, loading }) => {
+              if (loading) {
+                return <Text>Loading..</Text>;
+              }
+
+              return <Text>{data.me.email}</Text>;
+            }}
+          </Query>
+          <View>
+            <Ionicons
+              name={
+                OS === "ios"
+                  ? "ios-add-cirlce-outline"
+                  : "md-add-circle-outline"
+              }
+            />
+          </View>
         </View>
       </BlurView>
     </Modal>
@@ -28,6 +75,11 @@ export const BabyModal: React.SFC<Props> = ({ isOpen, closeModal }) => {
 };
 
 const styles = StyleSheet.create({
+  closeView: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end"
+  },
   modalViewContainer: {
     alignSelf: "center",
     marginTop: "auto",
