e7321d0041eefec7132d711180b7aada53643a40
datdevboi <cami172013@gmail.com>
2019-01-31 17:27:00 -0600

    switched to createUploadLink instead of our httplink. This package will help us with making uploads in graphql

--- app/src/client.ts
+++ app/src/client.ts
@@ -1,11 +1,12 @@
 import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
+
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { createUploadLink } from "apollo-upload-client";
 
 const host = "http://10.0.0.87:4000/graphql";
 
 export const client = new ApolloClient({
-  link: new HttpLink({
+  link: createUploadLink({
     uri: host,
     credentials: "include"
   }),
