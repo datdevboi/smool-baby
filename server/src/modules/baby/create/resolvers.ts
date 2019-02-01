36194cbf2ae56cf74a78aa700a04446dc033a2ad
datdevboi <cami172013@gmail.com>
2019-02-01 17:21:17 -0600

    created a few function that will store the picture sent by the user in a folder named pictures

--- server/src/modules/baby/create/resolvers.ts
+++ server/src/modules/baby/create/resolvers.ts
@@ -1,33 +1,59 @@
 import dayjs from "dayjs";
+import { createWriteStream } from "fs";
+import * as shortid from "shortid";
 import { GraphQLScalarType, Kind } from "graphql";
 import { IResolvers } from "apollo-server";
+
+const storeUpload = async (stream: any, mimetype: string): Promise<any> => {
+  // aseq2
+  const extension = mimetype.split("/")[1];
+  const id = `${shortid.generate()}.${extension}`;
+  const path = `images/${id}`;
+
+  return new Promise((resolve, reject) =>
+    stream
+      .pipe(createWriteStream(path))
+      .on("finish", () => resolve({ id, path }))
+      .on("error", reject)
+  );
+};
+
+const processUpload = async (upload: any) => {
+  const { stream, mimetype } = await upload;
+  const { id } = await storeUpload(stream, mimetype);
+  return id;
+};
 
 export const resolvers: IResolvers = {
   Mutation: {
     createBaby: async (_, args, { prisma, res, req }) => {
-      const query = `
-        mutation createBaby($name: String!, $dob: DateTime!, $userId: ID!) {
-          createBaby(data: {
-            name: $name,
-            dob: $dob,
-            parent: {
-              connect: {
-                id: $userId
-              }
-            }
-          }) {
-            name
-            dob
-          }
-        }
-      
-      `;
+      const pictureUrl = await processUpload(args.input.picture);
+      console.log(pictureUrl);
+      console.log(args.input.name);
 
-      const baby = await prisma.$graphql(query, {
-        name: args.input.name,
-        dob: args.input.dob,
-        userId: req.userId
-      });
+      // const query = `
+      //   mutation createBaby($name: String!, $dob: DateTime!, $userId: ID!) {
+      //     createBaby(data: {
+      //       name: $name,
+      //       dob: $dob,
+      //       parent: {
+      //         connect: {
+      //           id: $userId
+      //         }
+      //       }
+      //     }) {
+      //       name
+      //       dob
+      //     }
+      //   }
+
+      // `;
+
+      // const baby = await prisma.$graphql(query, {
+      //   name: args.input.name,
+      //   dob: args.input.dob,
+      //   userId: req.userId
+      // });
 
       return true;
     }
