264f8c015ee00cae377b2679350edd32b6b5ae46
datdevboi <cami172013@gmail.com>
2019-02-06 18:34:52 -0600

    configured our host variable and exported it

--- app/src/client.ts
+++ app/src/client.ts
@@ -3,11 +3,11 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createUploadLink } from "apollo-upload-client";
 
-const host = "http://10.0.0.87:4000/graphql";
+export const host = "http://10.0.0.87:4000";
 
 export const client = new ApolloClient({
   link: createUploadLink({
-    uri: host,
+    uri: `${host}/graphql`,
     credentials: "include"
   }),
   cache: new InMemoryCache()
