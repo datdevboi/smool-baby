5efea6426003e1dbb437d84d8333c883e055e073
datdevboi <cami172013@gmail.com>
2019-02-14 16:16:48 -0600

    created a basic color palette

--- app/src/config.ts
+++ app/src/config.ts
@@ -3,7 +3,13 @@ import { Dimensions, Platform } from "react-native";
 const { width, height } = Dimensions.get("window");
 
 export const CONFIG = {
-  colors: {},
+  colors: {
+    AZUREISH_WHITE: "rgba(214, 226, 249, 1)",
+    PALE_CORNFLOWER_BLUE: "rgba(171, 195, 255, 1);",
+    UBE: "rgba(133, 129, 185, 1)",
+    JORDY_BLUE: "rgba(143, 180, 245, 1)",
+    DARK_PASTEL_PURPLE: "rgba(143, 123, 213, 1)"
+  },
   DEVICE_WIDTH: width,
   DEVICE_HEIGHT: height,
   OS: Platform.OS
