4fa45bfda6b4e987b20adf5885ca1c1ffba74291
datdevboi <cami172013@gmail.com>
2019-02-15 16:52:11 -0600

    created a reusable PickerField component

---
+++ app/src/components/PickerField.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+import { Picker } from "react-native-ui-lib";
+import { FieldProps } from "formik";
+
+export class PickerField extends React.Component<
+  FieldProps<any> & {
+    value: string;
+    option: any;
+  }
+> {
+  onChange = (text: string) => {
+    this.props.form.setFieldValue(this.props.field.name, text);
+  };
+  render() {
+    const { field, form, value, options, ...props } = this.props;
+
+    return (
+      <Picker
+        {...props}
+        onChange={this.onChange}
+        error={form.errors[field.name]}
+        value={field.value}
+      >
+        {options.map(option => (
+          <Picker.Item key={option} value={option} />
+        ))}
+      </Picker>
+    );
+  }
+}
